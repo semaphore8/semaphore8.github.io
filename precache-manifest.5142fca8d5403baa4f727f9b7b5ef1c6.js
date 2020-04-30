9077c9c6e3238d79eb388aec193c64841beadf0f
sgbliznyuk <sgbliznyuk@gmail.com>
2020-04-30 13:31:51 +0300

    Updates

---
+++ precache-manifest.5142fca8d5403baa4f727f9b7b5ef1c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55137ab5cc448c1676b6a3e26acfdc10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "947926158a7f062a2000",
+    "url": "/static/css/main.c07f2411.chunk.css"
+  },
+  {
+    "revision": "8b60b6e0e7d2215a9ad5",
+    "url": "/static/js/2.ccef414e.chunk.js"
+  },
+  {
+    "revision": "4b8fe9661608bf0477df6e97e5789b57",
+    "url": "/static/js/2.ccef414e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "947926158a7f062a2000",
+    "url": "/static/js/main.6c2368f6.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
